891b393d3a6c3a9c1194fd20a7bba4702092a754
raja-ravi-prakash <iamraviprakash1@gmail.com>
2020-05-28 20:59:52 +0530

    linking

--- app/electron-app/main.js
+++ app/electron-app/main.js
@@ -5,16 +5,20 @@ const cors = require("cors");
 const { app, BrowserWindow, dialog } = electron;
 
 let window;
-app.on("ready", function() {
+app.on("ready", function () {
   window = new BrowserWindow({});
-  window.loadURL("http://localhost:8080");
+  window.loadURL(
+    process.argv[2] == "--prod"
+      ? "http://localhost:2155"
+      : "http://localhost:8080"
+  );
 });
 
 let expressApp = express();
 
 expressApp.use(express.static("public"));
 expressApp.use(cors());
-expressApp.post("/submit", function(req, res) {
+expressApp.post("/submit", function (req, res) {
   dialog.showMessageBox({
     buttons: ["Yes"],
     message: JSON.stringify(req.body),
